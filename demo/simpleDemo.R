986ce5054dc6b55782d67a7ae620dfa1d93183a5
johndharrison <johndharrison0@gmail.com>
2014-09-10 20:44:23 +0700

    Add a simple demo

---
+++ demo/simpleDemo.R
@@ -0,0 +1,30 @@
+library(shinyStorage)
+library(shiny)
+
+runApp(
+  list(
+    ui = fluidPage(
+      addSS(),
+      uiOutput("textExample")
+      )
+    , server = function(input, output, session){
+      ss <- shinyStore(session = session)
+      output$textExample <- renderUI({
+        myVar <- ss$get("myVar")
+        if(is.null(myVar)){
+          textInput("textID", "Add some text to local storage")
+        }else{
+          textInput("textID", "Add some text to local storage", myVar)          
+        }
+      })
+      
+      observe({
+        if(!is.null(input$textID)){
+          if(){
+            
+          }
+        }
+      })
+    }
+    )
+  )
